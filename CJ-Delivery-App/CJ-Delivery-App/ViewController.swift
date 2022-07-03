8fab576c74bb5cf4246268b21db28542da7c4d92
asd147asd147 <asd147asd147@naver.com>
2022-07-04 06:07:21 +0900

    init settings

---
+++ CJ-Delivery-App/CJ-Delivery-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CJ-Delivery-App
+//
+//  Created by 최원준 on 2022/07/04.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
